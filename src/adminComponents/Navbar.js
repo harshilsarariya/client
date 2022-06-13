e27ce166f7b18de8ee0942147b2b3c036327a894
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2022-06-13 20:45:41 +0530

    Navigation Position Updated

---
+++ src/adminComponents/Navbar.js
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const [profile, setProfile] = useState(false);
+
+  const widthOfNavbar = window.innerWidth - 64;
+  return (
+    <div>
+      <div className="w-full nunito-font h-full bg-[#F1F5F9]">
+        {/* Navigation starts */}
+        <nav className="h-16 flex items-center m-2 rounded-xl lg:items-stretch justify-end lg:justify-between bg-[#164E63] shadow relative z-10">
+          <div className="hidden lg:flex w-full pr-6">
+            <div className="w-1/2  hidden lg:flex items-center pl-3">
+              <h1 className="text-3xl text-white px-10">Ideal</h1>
+              <div className="relative w-full">
+                <div className="text-gray-500 absolute ml-4 inset-0 m-auto w-4 h-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-search"
+                    width={16}
+                    height={16}
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <circle cx={10} cy={10} r={7} />
+                    <line x1={21} y1={21} x2={15} y2={15} />
+                  </svg>
+                </div>
+                <input
+                  className="border border-gray-100 focus:outline-none focus:border-indigo-700 rounded w-full text-sm text-gray-500 bg-gray-100 pl-12 py-2"
+                  type="text"
+                  placeholder="Search"
+                />
+              </div>
+            </div>
+            <div className="w-1/2 hidden lg:flex">
+              <div className="w-full flex items-center pl-8 justify-end">
+                <div className="h-full w-20 flex items-center justify-center ">
+                  <div className="relative cursor-pointer text-[#688C9A]">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon icon-tabler icon-tabler-bell"
+                      width={28}
+                      height={28}
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" />
+                      <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
+                      <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
+                    </svg>
+                    <div className="w-2 h-2 rounded-full bg-red-400 border border-white absolute inset-0 mt-1 mr-1 m-auto" />
+                  </div>
+                </div>
+                <div
+                  className="flex items-center relative cursor-pointer"
+                  onClick={() => setProfile(!profile)}
+                >
+                  <div className="rounded-full">
+                    {profile ? (
+                      <ul className="p-2 w-52 border-r  bg-[#123E4F] absolute rounded right-0 shadow mt-12 sm:mt-16 ">
+                        <li className="flex w-full justify-between  p-1 rounded cursor-default  items-center">
+                          <div className="flex items-center flex-col ">
+                            <span className="text-base text-white ml-2">
+                              Harshil Sarariya
+                            </span>
+                            <span className="text-sm ml-2 text-[#91A6AD]">
+                              Software Engineer
+                            </span>
+                          </div>
+                        </li>
+                        <hr className="my-2 border-[#254D5D]" />
+                        <li className="flex w-full  justify-between text-white hover:bg-[#1E4858] p-1 rounded cursor-pointer items-center">
+                          <div className="flex items-center">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="icon icon-tabler icon-tabler-user"
+                              width={18}
+                              height={18}
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              fill="none"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            >
+                              <path stroke="none" d="M0 0h24v24H0z" />
+                              <circle cx={12} cy={7} r={4} />
+                              <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                            </svg>
+                            <span className="text-base ml-2">My Profile</span>
+                          </div>
+                        </li>
+                        <li className="flex w-full justify-between text-white hover:bg-[#1E4858] p-1 rounded  cursor-pointer items-center mt-2">
+                          <div className="flex items-center">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="icon icon-tabler icon-tabler-logout"
+                              width={20}
+                              height={20}
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              fill="none"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            >
+                              <path stroke="none" d="M0 0h24v24H0z" />
+                              <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                              <path d="M7 12h14l-3 -3m0 6l3 -3" />
+                            </svg>
+                            <span className="text-base ml-2">Sign out</span>
+                          </div>
+                        </li>
+                      </ul>
+                    ) : (
+                      ""
+                    )}
+                    <div className="relative">
+                      <img
+                        className="rounded-full h-10 w-10 object-cover"
+                        src="http://enigma-vue.left4code.com/assets/profile-9.30af9082.jpg"
+                        alt="avatar"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="text-gray-600 mr-8 visible lg:hidden relative"
+            onClick={() => setShow(!show)}
+          >
+            {show ? (
+              " "
+            ) : (
+              <svg
+                aria-label="Main Menu"
+                aria-haspopup="true"
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-menu cursor-pointer"
+                width={30}
+                height={30}
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <line x1={4} y1={8} x2={20} y2={8} />
+                <line x1={4} y1={16} x2={20} y2={16} />
+              </svg>
+            )}
+          </div>
+        </nav>
+        {/* Navigation ends */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/adminComponents/SideBar.js
@@ -0,0 +1,347 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const SideBar = () => {
+  const [dashboard, setDashboard] = useState("bg-white");
+  const [product, setProduct] = useState("text-[#5C697B] hover:bg-white");
+  const [performance, setPerformance] = useState(
+    " text-[#5C697B] hover:bg-white"
+  );
+  const [show, setShow] = useState(false);
+  const [profile, setProfile] = useState(false);
+  return (
+    <div>
+      {/* Sidebar starts */}
+      <div className="absolute  lg:relative w-64 h-screen shadow bg-[#E2E8F0] hidden lg:block ">
+        <ul aria-orientation="vertical" className="pt-2 -mt-2">
+          <li
+            className={`p-4 cursor-pointer  text-base leading-3 tracking-normal  m-3 rounded-xl ${dashboard} transition`}
+          >
+            <Link to={"/ideal-admin"}>
+              <div
+                className="flex items-center  "
+                onClick={() => {
+                  setDashboard("bg-white font-[#316275] ");
+                  setProduct("text-[#5C697B] hover:bg-white");
+                  setPerformance("text-[#5C697B] hover:bg-white");
+                }}
+              >
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-grid"
+                    width={20}
+                    height={20}
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <rect x={4} y={4} width={6} height={6} rx={1} />
+                    <rect x={14} y={4} width={6} height={6} rx={1} />
+                    <rect x={4} y={14} width={6} height={6} rx={1} />
+                    <rect x={14} y={14} width={6} height={6} rx={1} />
+                  </svg>
+                </div>
+                <span className="ml-2">Dashboard</span>
+              </div>
+            </Link>
+          </li>
+          <li
+            className={`p-4 cursor-pointer  text-base leading-3 tracking-normal  m-3 rounded-xl ${product} transition`}
+          >
+            <Link to={"/view-complaints"}>
+              <div
+                className="flex items-center"
+                onClick={() => {
+                  setProduct("bg-white");
+                  setDashboard("text-[#5C697B] hover:bg-white");
+                  setPerformance("text-[#5C697B] hover:bg-white");
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-puzzle"
+                  width={20}
+                  height={20}
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" />
+                  <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1" />
+                </svg>
+                <span className="ml-2">View Complaints</span>
+              </div>
+            </Link>
+          </li>
+          <li
+            className={`p-4 cursor-pointer  text-base leading-3 tracking-normal  m-3 rounded-xl ${performance} transition`}
+          >
+            <Link
+              to={"/users"}
+              className="flex items-center"
+              onClick={() => {
+                setPerformance("bg-white");
+                setProduct("text-[#5C697B] hover:bg-white");
+                setDashboard("text-[#5C697B] hover:bg-white");
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-compass"
+                width={20}
+                height={20}
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <polyline points="8 16 10 10 16 8 14 14 8 16" />
+                <circle cx={12} cy={12} r={9} />
+              </svg>
+              <span className="ml-2">Users</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      {/*Mobile responsive sidebar*/}
+      <div
+        className={
+          show
+            ? "w-full h-full absolute z-40  transform  translate-x-0 "
+            : "   w-full h-full absolute z-40  transform -translate-x-full"
+        }
+        id="mobile-nav"
+      >
+        <div
+          className="bg-gray-800 opacity-50 absolute h-full w-full lg:hidden"
+          onClick={() => setShow(!show)}
+        />
+        <div className="absolute z-40 sm:relative w-64 md:w-96 shadow pb-4 bg-gray-100 lg:hidden transition duration-150 ease-in-out h-full">
+          <div className="flex flex-col justify-between h-full w-full">
+            <div>
+              <div className="flex items-center justify-between px-8">
+                <div className="h-16 w-full flex items-center">
+                  <h1 className="text-3xl text-black">Ideal</h1>
+                </div>
+                <div
+                  id="closeSideBar"
+                  className="flex items-center justify-center h-10 w-10"
+                  onClick={() => setShow(!show)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-x"
+                    width={20}
+                    height={20}
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <line x1={18} y1={6} x2={6} y2={18} />
+                    <line x1={6} y1={6} x2={18} y2={18} />
+                  </svg>
+                </div>
+              </div>
+              <ul aria-orientation="vertical" className=" py-6">
+                <li className="pl-6  cursor-pointer text-black text-3xl leading-3 tracking-normal pb-4 pt-5 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
+                  <div className="flex items-center">
+                    <div className="w-6 h-6 md:w-8 md:h-8">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-grid"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <rect x={4} y={4} width={6} height={6} rx={1} />
+                        <rect x={14} y={4} width={6} height={6} rx={1} />
+                        <rect x={4} y={14} width={6} height={6} rx={1} />
+                        <rect x={14} y={14} width={6} height={6} rx={1} />
+                      </svg>
+                    </div>
+                    <span className="ml-2 xl:text-base md:text-2xl text-base">
+                      Dashboard
+                    </span>
+                  </div>
+                </li>
+                <li className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-4 mb-4 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
+                  <div className="flex items-center">
+                    <div className="w-6 h-6 md:w-8 md:h-8">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-puzzle"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1" />
+                      </svg>
+                    </div>
+                    <span className="ml-2 xl:text-base md:text-2xl text-base">
+                      Products
+                    </span>
+                  </div>
+                </li>
+                <li className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mb-4 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
+                  <div className="flex items-center">
+                    <div className="w-6 h-6 md:w-8 md:h-8">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-compass"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <polyline points="8 16 10 10 16 8 14 14 8 16" />
+                        <circle cx={12} cy={12} r={9} />
+                      </svg>
+                    </div>
+                    <span className="ml-2 xl:text-base md:text-2xl text-base">
+                      Performance
+                    </span>
+                  </div>
+                </li>
+                <li className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
+                  <div className="flex items-center">
+                    <div className="w-6 h-6 md:w-8 md:h-8">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-code"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <polyline points="7 8 3 12 7 16" />
+                        <polyline points="17 8 21 12 17 16" />
+                        <line x1={14} y1={4} x2={10} y2={20} />
+                      </svg>
+                    </div>
+                    <span className="ml-2 xl:text-base md:text-2xl text-base">
+                      Deliverables
+                    </span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="w-full">
+              <div className="flex justify-center mb-4 w-full px-6">
+                <div className="relative w-full">
+                  <div className="text-gray-500 absolute ml-4 inset-0 m-auto w-4 h-4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon icon-tabler icon-tabler-search"
+                      width={16}
+                      height={16}
+                      viewBox="0 0 24 24"
+                      strokeWidth={1}
+                      stroke="#A0AEC0"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" />
+                      <circle cx={10} cy={10} r={7} />
+                      <line x1={21} y1={21} x2={15} y2={15} />
+                    </svg>
+                  </div>
+                  <input
+                    className="bg-gray-200 focus:outline-none rounded w-full text-sm text-gray-500  pl-10 py-2"
+                    type="text"
+                    placeholder="Search"
+                  />
+                </div>
+              </div>
+              <div className="border-t border-gray-300">
+                <div className="w-full flex items-center justify-between px-6 pt-1">
+                  <div className="flex items-center">
+                    <img
+                      alt="profile-pic"
+                      src="https://tuk-cdn.s3.amazonaws.com/assets/components/boxed_layout/bl_1.png"
+                      className="w-8 h-8 rounded-md"
+                    />
+                    <p className="md:text-xl text-gray-800 text-base leading-4 ml-2">
+                      Jane Doe
+                    </p>
+                  </div>
+                  <ul className="flex">
+                    <li className="cursor-pointer text-white pt-5 pb-3">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-messages"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        strokeWidth={1}
+                        stroke="#718096"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
+                        <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
+                      </svg>
+                    </li>
+                    <li className="cursor-pointer text-white pt-5 pb-3 pl-3">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-bell"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        strokeWidth={1}
+                        stroke="#718096"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
+                        <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
+                      </svg>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*Mobile responsive sidebar*/}
+      {/* Sidebar ends */}
+    </div>
+  );
+};
+
+export default SideBar;
