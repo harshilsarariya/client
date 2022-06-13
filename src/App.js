a3464ad75b4e36a3cbb6bcf5a529b915619895ba
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2022-06-13 20:46:18 +0530

    Navigation Routed Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import SignIn from "./components/SignIn";
 import ComplaintForm from "./components/ComplaintForm";
+import AdminHome from "./adminComponents/AdminHome";
+import UpdateComplaint from "./adminComponents/UpdateComplaint";
+import ViewComplaint from "./adminComponents/ViewComplaints";
+import ViewComplaintDetails from "./adminComponents/ViewComplaintDetails";
+import Users from "./adminComponents/Users";
 
 function App() {
   return (
@@ -12,6 +16,14 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/register-complaint" element={<ComplaintForm />} />
+        <Route path="/ideal-admin" element={<AdminHome />} />
+        <Route path="/view-complaints" element={<ViewComplaint />} />
+        <Route
+          path="/view-details-complaint"
+          element={<ViewComplaintDetails />}
+        />
+        <Route path="/update-complaint" element={<UpdateComplaint />} />
+        <Route path="/users" element={<Users />} />
       </Routes>
     </Router>
   );
