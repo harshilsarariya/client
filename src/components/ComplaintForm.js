09cfeea6598109efadd93320476d5394d517d92d
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2022-06-23 15:38:51 +0530

    url changed

--- src/components/ComplaintForm.js
+++ src/components/ComplaintForm.js
@@ -28,8 +28,8 @@ const ComplaintForm = (props) => {
     e.preventDefault();
     // API Call
     const response = await fetch(
-      // "https://ideal-server.herokuapp.com/api/complaint/addcomplaint",
-      "http://localhost:5000/api/complaint/addcomplaint",
+      "https://ideal-server.herokuapp.com/api/complaint/addcomplaint",
+      // "http://localhost:5000/api/complaint/addcomplaint",
       {
         method: "POST",
         headers: {
